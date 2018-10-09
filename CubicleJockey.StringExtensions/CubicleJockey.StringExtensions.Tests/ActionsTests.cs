43187cdc24d5acab9b12fdd407b3dd0a3671664b
André Davis <cubicle.jockey@live.com>
2018-10-09 10:31:32 -0700

    First Action

--- CubicleJockey.StringExtensions/CubicleJockey.StringExtensions.Tests/ActionsTests.cs
+++ CubicleJockey.StringExtensions/CubicleJockey.StringExtensions.Tests/ActionsTests.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Console;
 
 namespace CubicleJockey.StringExtensions.Tests
 {
     [TestClass]
     public class ActionsTests
     {
+        [TestMethod]
+        public void ForEach()
+        {
+            //Arrange
+            var count = 0;
+            const string VALUE = "123456789";
+
+            void DoWork(char character)
+            {
+                WriteLine($"Counting Character {character}.");
+                count++;
+            }
+
+            //Act
+            VALUE.ForEach(DoWork);
+
+            //Assert
+            count.Should().Be(9);
+        }
     }
 }
--- CubicleJockey.StringExtensions/CubicleJockey.StringExtensions/Actions.cs
+++ CubicleJockey.StringExtensions/CubicleJockey.StringExtensions/Actions.cs
@@ -1,6 +1,15 @@
-﻿namespace CubicleJockey.StringExtensions
+﻿using System;
+
+namespace CubicleJockey.StringExtensions
 {
     public static class Actions
     {
+        public static void ForEach(this string self, Action<char> action)
+        {
+            foreach (var character in self)
+            {
+                action(character);
+            }
+        }
     }
 }
