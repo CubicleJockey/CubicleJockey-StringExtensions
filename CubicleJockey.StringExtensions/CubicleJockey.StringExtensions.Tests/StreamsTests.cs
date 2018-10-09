2da51afd59ddeff4f893b37e969ae2783cc693ea
André Davis <cubicle.jockey@live.com>
2018-10-09 11:19:58 -0700

    ToStream

---
+++ CubicleJockey.StringExtensions/CubicleJockey.StringExtensions.Tests/StreamsTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace CubicleJockey.StringExtensions.Tests
+{
+    [TestClass]
+    public class StreamsTests
+    {
+        [TestMethod]
+        public void ToStreamDefaultEncoding()
+        {
+            TestStreamExtension();
+        }
+
+
+
+        [TestMethod]
+        public void ToStreamNonDefaultEncoding()
+        {
+            TestStreamExtension(Encoding.BigEndianUnicode);
+        }
+
+        private static void TestStreamExtension(Encoding encoding = null)
+        {
+            //Arrange
+            const string WORKSTRING = "I am a string to be turned into a stream.";
+
+            //Act
+            var streamInfo = encoding == null ? WORKSTRING.ToStream() : WORKSTRING.ToStream(encoding);
+            var expectedEncoding = encoding ?? Encoding.UTF8;
+
+            //Assert
+            streamInfo.Should().NotBeNull();
+            streamInfo.Encoding.Should().Be(expectedEncoding);
+            streamInfo.Stream.Should().NotBeNull();
+
+            var memoryStream = new MemoryStream();
+            streamInfo.Stream.CopyTo(memoryStream);
+
+            var result = expectedEncoding.GetString(memoryStream.ToArray());
+
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be(WORKSTRING);
+        }
+    }
+}
---
+++ CubicleJockey.StringExtensions/CubicleJockey.StringExtensions/Streams.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+namespace CubicleJockey.StringExtensions
+{
+    public static class Streams
+    {
+        /// <summary>
+        /// Convert a string to a Stream
+        /// </summary>
+        /// <param name="self">A String</param>
+        /// <param name="encoding">Encoding style for the stream. Defaults to UTF8 if not provided.</param>
+        /// <returns>A Stream and its current encoding</returns>
+        public static (Stream Stream, Encoding Encoding) ToStream(this string self, Encoding encoding = null)
+        {
+            if (encoding == null) { encoding = Encoding.UTF8; }
+            var bytes = encoding.GetBytes(self);
+            return (new MemoryStream(bytes), encoding);
+        }
+    }
+}
