8b1e4565dd28c379abfba52cac70172a1342ce32
André Davis <cubicle.jockey@live.com>
2018-10-10 13:21:13 -0700

    Checkpoint

---
+++ CubicleJockey.StringExtensions/CubicleJockey.StringExtensions/Encryption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace CubicleJockey.StringExtensions
+{
+    public static class Encryption
+    {
+        public static string ToBase64(this string self, Encoding encoding = null)
+        {
+            if (encoding == null) { encoding = Encoding.UTF8; }
+
+            var bytes = encoding.GetBytes(self);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
